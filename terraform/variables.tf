70e8055a7cf91cf34ad703590d57a908ad84f824
Sergei Morozov <sergei.sm.morozov@gmail.com>
2025-06-21 23:36:16 +0400

    feat(terraform): add VPC

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
   type        = string
   default     = "eu-central-1"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
